7a340f31cb663fc899c5307b67d195e7c4d35f3a
Paczek-12 <adam.kochaniecki@gmail.com>
2025-01-26 11:33:21 +0100

    fullscreen toggle (on F)

--- scripts/global.gd
+++ scripts/global.gd
@@ -1,2 +1,10 @@
 extends Node
 # Ten skrypt działa ZAWSZE
+
+func _input(event):
+	if event.is_action_pressed("ToggleFullscreen"):
+		var isFullscreen = DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN
+		
+		DisplayServer.window_set_mode(
+			DisplayServer.WINDOW_MODE_WINDOWED if isFullscreen 
+			else DisplayServer.WINDOW_MODE_FULLSCREEN)
