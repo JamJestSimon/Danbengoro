b81acc695cc48cc3546339bd426413b52ccfc1db
Paczek-12 <adam.kochaniecki@gmail.com>
2025-01-26 14:06:49 +0100

    credits, empty jsons

--- MainMenu/main_menu.gd
+++ MainMenu/main_menu.gd
@@ -3,8 +3,21 @@ extends Control
 @export
 var gameScene: PackedScene
 
+@export var credits: CanvasLayer
+
+func _ready():
+	credits.visible = false
+
 func _on_start_button_down():
 	get_tree().change_scene_to_packed(gameScene)
 
 func _on_quit_button_down():
 	get_tree().quit()
+
+
+func _on_credits_butt_button_down():
+	credits.visible = true
+	get_node("%CreditsButt").release_focus()
+
+func _on_credits_butt_2_button_down():
+	credits.visible = false
