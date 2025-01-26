486b5a268a12cfdb8242815cd45c328431ed6ec1
Paczek-12 <adam.kochaniecki@gmail.com>
2025-01-26 12:01:58 +0100

    pause menu adjustments

--- scripts/pause_menu.gd
+++ scripts/pause_menu.gd
@@ -5,7 +5,7 @@ func _ready():
 
 func _input(event):
 	
-	if event.is_action_pressed("Pause"):
+	if event.is_action_pressed("Pause") and is_equal_approx(1., Engine.time_scale):
 		togglePause()
 
 func _on_resume_button_down():
@@ -14,6 +14,7 @@ func _on_resume_button_down():
 func togglePause():
 	var newPauseVal: bool = not get_tree().paused
 	get_tree().paused = newPauseVal
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE if newPauseVal else Input.MOUSE_MODE_HIDDEN)
 	visible = newPauseVal
 
 func _on_to_menu_button_down():
